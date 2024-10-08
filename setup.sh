0e3d749238b414e7993abf1fc56e380da622e73d
Ashton Honnecke <ashton@pixelstub.com>
2024-10-08 11:43:29 -0600

    init

---
+++ setup.sh
@@ -0,0 +1,6 @@
+mkdir apps backend shared amplify
+mkdir apps/react-app-1 apps/react-app-2
+mkdir backend/service-1 backend/service-2
+mkdir shared/utils shared/components
+mkdir amplify/backend amplify/frontend
+touch amplify.yml
