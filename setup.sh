258cf3a2e79866ceeac49ed63a3c93c6e34064c4
Ashton Honnecke <ashton@pixelstub.com>
2024-10-08 11:50:31 -0600

    create .gitignores

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ mkdir backend/service-1 backend/service-2
 mkdir shared/utils shared/components
 mkdir amplify/backend amplify/frontend
 touch amplify.yml
+touch amplify/backend/.gitignore amplify/frontend/.gitignore apps/react-app-1/.gitignore apps/react-app-2/.gitignore backend/service-1/.gitignore backend/service-2/.gitignore shared/components/.gitignore shared/utils/.gitignore
